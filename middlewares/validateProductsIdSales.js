65051c3630fdb295c77dfa21ad05c4089c7312cd
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-25 18:57:30 -0300

    Refatorando - Removendo o try catch

--- middlewares/validateProductsIdSales.js
+++ middlewares/validateProductsIdSales.js
@@ -1,11 +1,7 @@
 const validateProductsIdSales = (req, res, next) => {
-  try {
     const [sale] = req.body;
     if (!sale.productId) return res.status(400).json({ message: '"productId" is required' });
     next();
-  } catch (error) {
-    next(error);
-  }
 };
 
 module.exports = validateProductsIdSales;
