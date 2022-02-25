dcbf5c4c7f7d43679f3b282cba4f683dc53efab1
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-25 17:52:16 -0300

    Finalizando Requisito 3

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const productsController = require('./controllers/productsController');
 const productIdController = require('./controllers/productIdController');
 const salesController = require('./controllers/salesController');
 const saleIdController = require('./controllers/saleIdController');
+const { 
+  validateNameProducts, 
+  validateQuantityProducts, 
+  validateProductsIdSales,
+  validateQuantitySales,
+ } = require('./middlewares');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,8 +22,14 @@ app.get('/', (_request, response) => {
 app.get('/products', productsController.getAll);
 app.get('/products/:id', productIdController.getById);
 
+app.post('/products', validateNameProducts, validateQuantityProducts);
+app.put('/products/:id', validateNameProducts, validateQuantityProducts);
+
 app.get('/sales', salesController.getAll);
 app.get('/sales/:id', saleIdController.getById);
+
+app.post('/sales', validateProductsIdSales, validateQuantitySales);
+app.put('/sales/:id', validateProductsIdSales, validateQuantitySales);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
