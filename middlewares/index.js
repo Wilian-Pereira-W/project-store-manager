284e873501b16b9384806da95d4451d5abcbe671
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-25 17:46:25 -0300

    Adicionando as validações de produtos e vendas

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,14 @@
 const serialize = require('./serialize');
 const serializeById = require('./serializeById');
+const validateNameProducts = require('./validateNameProducts');
+const validateQuantityProducts = require('./validateQuantityProducts');
+const validateProductsIdSales = require('./validateProductsIdSales');
+const validateQuantitySales = require('./validateQuantitySales');
 
-module.exports = { serialize, serializeById };
+module.exports = { 
+  serialize, 
+  serializeById, 
+  validateNameProducts, 
+  validateQuantityProducts, 
+  validateProductsIdSales,
+  validateQuantitySales };
