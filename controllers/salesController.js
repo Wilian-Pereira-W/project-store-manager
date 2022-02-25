8b0bf984d248b270f4e80914085ad6d9bfa54149
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-25 12:50:30 -0300

    Renomeando o nome da const

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -2,8 +2,8 @@ const salesServices = require('../services/salesServices');
 
 const getAll = async (_req, res, next) => {
   try {
-    const products = await salesServices.getAll();
-    return res.status(200).json(products);
+    const sales = await salesServices.getAll();
+    return res.status(200).json(sales);
   } catch (error) {
     next(error);
   }
