c219091752ac79e2c366d920d9a6157ee97d173b
Wilian Pereira <wilian_pereira92@outlook.com>
2022-02-24 21:20:19 -0300

    Implementando model do sales

---
+++ models/salesModel.js
@@ -0,0 +1,29 @@
+const connection = require('./connection');
+const serializeMiddlewares = require('../middlewares/serialize');
+
+const getAll = async () => {
+  const SELECT = `SELECT saleProducts.sale_id, sale.date,  saleProducts.product_id, 
+  saleProducts.quantity`;
+  const FROM = 'FROM StoreManager.sales AS sale';
+  const INNERJOIN = 'INNER JOIN StoreManager.sales_products AS saleProducts';
+  const ON = 'ON sale.id = saleProducts.sale_id';
+  const ORDERBY = 'ORDER BY saleProducts.sale_id, saleProducts.product_id'; 
+  const [sales] = await connection
+  .execute(`${SELECT} ${FROM} ${INNERJOIN} ${ON} ${ORDERBY}`);
+  return sales.map(serializeMiddlewares.serialize);
+}; 
+
+const getById = async (id) => {
+  const SELECT = `SELECT saleProducts.sale_id, sale.date,  saleProducts.product_id, 
+  saleProducts.quantity`;
+  const FROM = 'FROM StoreManager.sales AS sale';
+  const INNERJOIN = 'INNER JOIN StoreManager.sales_products AS saleProducts';
+  const ON = 'ON sale.id = saleProducts.sale_id';
+  const WHERE = 'WHERE saleProducts.sale_id = ?';
+  const ORDERBY = 'ORDER BY saleProducts.sale_id, saleProducts.product_id'; 
+  const [sales] = await connection
+  .execute(`${SELECT} ${FROM} ${INNERJOIN} ${ON} ${WHERE} ${ORDERBY}`, [id]);
+  return sales.map(serializeMiddlewares.serializeId);
+};
+
+module.exports = { getAll, getById };
